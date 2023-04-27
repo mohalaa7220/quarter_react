1f2138e46e509a45e419e51d399f3d4aa8559394
Mohamed Alaa <ma722072207220@gmail.com>
2023-04-27 14:23:40 +0200

    updated

---
+++ src/components/ProductsHome/ProductsHome.js
@@ -0,0 +1,39 @@
+import React, { useEffect, lazy, Suspense } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../state/productSlice";
+import SkeletonLoading from "../LazyLoaing/SkeletonLoading";
+const ProductsParent = lazy(() => import("../Products/Products"));
+
+const ProductsHome = () => {
+  const dispatch = useDispatch();
+  const { data, loading } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return (
+    <section className="featured pt-7">
+      <div className="container">
+        <div
+          class="services-heading text-center aos-init aos-animate"
+          data-aos="fade-up"
+        >
+          <h6 class="sec-heading">Properties</h6>
+          <h1>Featured Listings</h1>
+        </div>
+        {loading ? (
+          <SkeletonLoading />
+        ) : (
+          <div className="row">
+            <Suspense fallback={<SkeletonLoading />}>
+              <ProductsParent data={data.slice(0, 6)} />
+            </Suspense>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ProductsHome;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
-
+import ProductsHome from "../components/ProductsHome/ProductsHome";
 const Home = () => {
   return (
     <>
       <Banner />
+      <ProductsHome />
     </>
   );
 };
